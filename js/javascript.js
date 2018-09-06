33f4a37a1118aacfd73fe2aa3ece5fa952702954
ethan.daniels <e.daniels20@live.com>
2018-09-06 09:41:19 -0500

    created the github io protfolio

---
+++ js/javascript.js
@@ -0,0 +1,50 @@
+function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+r(function() {
+    document.getElementById('email').onclick = function() {
+        var textArea = document.createElement("textarea");
+        textArea.value = 'e.daniels20@live.com';
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);x
+        alert('Copied To clipboard')
+    }
+})
+// $(document).ready(function () {
+//     console.log('Sup');
+
+//     $( '.about-button' ).click(function (event) {
+//         console.log('about');
+//         event.preventDefault();
+
+//         $( '.about' ).css( 'display', 'block' );
+//         $( '.projects' ).css( 'display', 'none');
+//         $( '.contact' ).css( 'display', 'none');
+//         $( '.about-button' ).addClass( 'nav-after' );
+//         $( '.projects-button' ).removeClass( 'nav-after' );
+//         $( '.contact-button' ).removeClass( 'nav-after' );
+//     })
+//     $( '.projects-button' ).click(function (event) {
+//         console.log('projects');
+//         event.preventDefault();
+
+//         $( '.projects' ).css( 'display' , 'block');
+//         $( '.about' ).css( 'display', 'none' );
+//         $( '.contact' ).css( 'display', 'none');
+//         $( '.projects-button' ).addClass( 'nav-after' );
+//         $( '.about-button' ).removeClass( 'nav-after' );
+//         $( '.contact-button' ).removeClass( 'nav-after' );
+//     })
+//     $( '.contact-button' ).click(function (event) {
+//         console.log('contact');
+//         event.preventDefault();
+
+//         $( '.contact' ).css( 'display', 'block');
+//         $( '.about' ).css( 'display', 'none' );
+//         $( '.projects' ).css( 'display', 'none' );
+//         $( '.contact-button' ).addClass( 'nav-after' );
+//         $( '.about-button' ).removeClass( 'nav-after' );
+//         $( '.projects-button' ).removeClass( 'nav-after' );
+//     })
+// })
